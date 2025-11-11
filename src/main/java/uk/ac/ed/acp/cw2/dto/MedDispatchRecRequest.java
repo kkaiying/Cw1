aaa92fe0b427050e0e7870094c0898304d05f6e4
kkaiying <kaiying.tey@gmail.com>
2025-11-11 11:20:24 +0000

    2a & 2b added

--- src/main/java/uk/ac/ed/acp/cw2/dto/MedDispatchRecRequest.java
+++ src/main/java/uk/ac/ed/acp/cw2/dto/MedDispatchRecRequest.java
@@ -1,6 +1,14 @@
 package uk.ac.ed.acp.cw2.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class MedDispatchRecRequest {
     public int id;
-
+    public LocalDate date;
+    public LocalTime time;
+    public RequirementsDTO requirements;
+    public boolean cooling;
+    public boolean heating;
+    public double maxCost;
 }
